2a3a05c4c19e5c829b149648391596dddd01cc64
exaul <edwin.falcones@tecnologicoliceocristiano.edu.ec>
2023-11-07 20:55:20 -0500

    validaciones

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -10,10 +10,37 @@
             BindingContext = MyEstudiante;
         }
 
+
+
         private void ConfirmarBtn_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Registro",MyEstudiante.Nombres +" "+ "Sus datos se ingresaron correctamente","Ok");
-
+            if (IsValid())
+            {
+                //DisplayAlert("Registro",MyEstudiante.Nombres +" "+ "Sus datos se ingresaron correctamente","Ok");
+                var mensaje = $"{MyEstudiante.Nombres} sus datos se ingresaron correctamente";
+                var titulo = "Registro";
+                DisplayAlert(titulo, mensaje, "oK");
+            }
+            
+        }
+        private bool IsValid()
+        {
+            if(string.IsNullOrEmpty(MyEstudiante.Nombres))
+            {
+                DisplayAlert("Registro Fallido", "el campo Nombre esta vacio", "ok");  
+                return false; 
+            }
+            if (string.IsNullOrEmpty(MyEstudiante.Apellidos))
+            {
+                DisplayAlert("Registro Fallido", "el campo Apellidos esta vacio", "ok");
+                return false;
+            }
+            if (string.IsNullOrEmpty(MyEstudiante.Materia))
+            {
+                DisplayAlert("Registro Fallido", "el campo Materia esta vacio", "ok");
+                return false;
+            }
+            return true;
         }
     }
 }
